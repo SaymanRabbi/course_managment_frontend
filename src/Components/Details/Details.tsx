bbe94b3800251ddd7355bb9257ab96d4bd5b1199
mousumiaktar <ridimarahmanmou5804@gmail.com>
2024-02-08 16:49:56 +0600

    details page

---
+++ src/Components/Details/Details.tsx
@@ -0,0 +1,82 @@
+import { BiVideo } from "react-icons/bi";
+const Details = () => {
+  const content = [
+    {
+      id: 1,
+      name: "150+ videos in 15 modules",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, rem facilis! Dolor nam quaerat nemo asperiores blanditiis non, corporis commodi.",
+    },
+    {
+      id: 2,
+      name: "150+ videos in 15 modules",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, rem facilis! Dolor nam quaerat nemo asperiores blanditiis non, corporis commodi.",
+    },
+    {
+      id: 3,
+      name: "150+ videos in 15 modules",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, rem facilis! Dolor nam quaerat nemo asperiores blanditiis non, corporis commodi.",
+    },
+    {
+        id: 4,
+        name: "150+ videos in 15 modules",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, rem facilis! Dolor nam quaerat nemo asperiores blanditiis non, corporis commodi.",
+      },
+      {
+        id: 5,
+        name: "150+ videos in 15 modules",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, rem facilis! Dolor nam quaerat nemo asperiores blanditiis non, corporis commodi.",
+      },
+      {
+        id: 6,
+        name: "150+ videos in 15 modules",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, rem facilis! Dolor nam quaerat nemo asperiores blanditiis non, corporis commodi.",
+      },
+  ];
+  return (
+    <div>
+      <div>
+        <img
+          className="h-32 mx-auto"
+          src="https://learnwithsumit.com/assets/images/svgs/peep1.svg"
+          alt=""
+        />
+        <h4 className="text-2xl lg:text-4xl font-bold text-textPrimary py-4 text-center">
+          Our Curses at a glance
+        </h4>
+        <p className="text-textPrimary text-center text-base">
+          What will be in this course
+        </p>
+      </div>
+      {/* Course Elements */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-bgPrimary/20 border-[.5px] border-bgPrimary rounded-[10px] my-4 p-4">
+        {content.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="border-b border-r border-bgPrimary p-8"
+            >
+              <BiVideo
+                size={50}
+                className="mx-auto bg-gradient-to-r text-[#384fde]"
+              />
+              <p className="font-bold text-textPrimary text-center">
+                {item.name}
+              </p>
+              <p className="text-textSecondary text-center text-sm mt-2 leading-8">
+                {item.description}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Details;
--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,5 +1,6 @@
 import HomeCard from "../Components/Card/HomeCard";
 import Container from "../Components/Container/Container";
+import Details from "../Components/Details/Details";
 import Hero from "../Components/Hero/Hero";
 
 const Home = () => {
@@ -8,6 +9,9 @@ const Home = () => {
       <Hero />
       <div className="mt-12 flex flex-col gap-10 lg:mt-0 lg:flex-row lg:gap-24 w-[90%] mx-auto">
         <HomeCard />
+      </div>
+      <div className="mt-20 w-[90%] mx-auto">
+        <Details />
       </div>
     </Container>
   );

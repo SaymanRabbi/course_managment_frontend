b9209dd6ca21d576b3ad626d95f70777407be5b7
sayman rabbi <mdrabbi.asm@gmail.com>
2024-02-08 12:20:24 +0600

    hero section add

--- src/utils/Timer.tsx
+++ src/utils/Timer.tsx
@@ -1,33 +1,38 @@
 import React, { useState, useEffect } from "react";
 
 interface CourseAvailabilityTimerProps {
-  courseStartDate: Date; // Replace with the actual start date of your course
+  courseStartDate: Date;
 }
 
 const CourseAvailabilityTimer: React.FC<CourseAvailabilityTimerProps> = ({
   courseStartDate,
 }) => {
-  const [remainingTime, setRemainingTime] = useState<number>(
-    calculateRemainingTime()
+  const [remainingTime, setRemainingTime] = useState<number>(() =>
+    calculateRemainingTime(courseStartDate)
   );
 
   useEffect(() => {
+    const storedRemainingTime = localStorage.getItem("remainingTime");
+
+    if (storedRemainingTime) {
+      setRemainingTime(Number(storedRemainingTime));
+    }
+
     const interval = setInterval(() => {
-      setRemainingTime(calculateRemainingTime());
+      setRemainingTime((prevRemainingTime) => {
+        const newRemainingTime = Math.max(0, prevRemainingTime - 1);
+        localStorage.setItem("remainingTime", String(newRemainingTime));
+        return newRemainingTime;
+      });
     }, 1000);
 
-    // Cleanup function to clear the interval when the component unmounts
     return () => clearInterval(interval);
-  }, []);
+  }, [courseStartDate]);
 
-  function calculateRemainingTime(): number {
+  function calculateRemainingTime(startDate: Date): number {
     const currentDate = new Date();
-    const timeDifference = courseStartDate.getTime() - currentDate.getTime();
-
-    // Calculate remaining time in seconds
-    const remainingSeconds = Math.floor(timeDifference / 1000);
-
-    return remainingSeconds;
+    const timeDifference = startDate.getTime() - currentDate.getTime();
+    return Math.max(0, Math.floor(timeDifference / 1000));
   }
 
   function formatTime(seconds: number): string {
@@ -35,14 +40,15 @@ const CourseAvailabilityTimer: React.FC<CourseAvailabilityTimerProps> = ({
     const hours = Math.floor((seconds % (3600 * 24)) / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = seconds % 60;
-
     return `${days} days, ${hours} hours, ${minutes} minutes, ${remainingSeconds} seconds`;
   }
 
   return (
-    <div className=" text-textPrimary text-[16px]">
+    <div>
       {remainingTime > 0 ? (
-        <h2>{formatTime(remainingTime)}</h2>
+        <h2 className=" text-textPrimary text-[16px]">
+          {formatTime(remainingTime)}
+        </h2>
       ) : (
         <h2>Course is no longer available.</h2>
       )}

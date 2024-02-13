34317f31178f9335838f1beecaa3c0ac29133e0a
mousumiaktar <ridimarahmanmou5804@gmail.com>
2024-02-13 19:03:23 +0600

    instructor

--- src/Components/Dashboard/AdminDashboard/PopularInstructor.tsx
+++ src/Components/Dashboard/AdminDashboard/PopularInstructor.tsx
@@ -7,36 +7,77 @@ import { HiUsers } from "react-icons/hi2";
 const PopularInstructor = () => {
   return (
     <DashboardCard title="Popular Instructors">
-      <div>
-        {popularInstructors.map((item) => {
-          return (
-            <div className="mt-8">
-              <div className="md:flex items-center gap-4">
-                <img className="h-12" src={item.img} alt="" />
-                <div className="text-textPrimary">
-                  <div>
-                    <p className="font-bold">{item.name}</p>
-                  </div>
-                  <div className="flex items-center gap-10">
-                    <div className="md:flex items-center gap-2">
-                      <HiUsers />
-                      <p>{item.students} students</p>
-                    </div>
-                    <div className="md:flex items-center gap-2">
-                      <BiSolidVideos />
-                      <p>{item.courses} courses</p>
-                    </div>
-                    <div className="md:flex items-center gap-2">
-                      <MdReviews />
-                      <p>{item.courses} reviews</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full h-[2px] bg-gray-700 mt-2"></div>
-            </div>
-          );
-        })}
+      <div className=" grid grid-cols-12">
+        <div className=" col-span-12">
+          <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left ">
+              <thead className=" font-bold text-textPrimary text-[16px]">
+                <tr>
+                  <th scope="col" className="px-6 py-3 rounded-s-lg">
+                    image
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Name
+                  </th>
+                  <th scope="col" className="px-6 py-3 rounded-e-lg">
+                    Students
+                  </th>
+                  <th scope="col" className="px-6 py-3 rounded-e-lg">
+                    Courses
+                  </th>
+                  <th scope="col" className="px-6 py-3 rounded-e-lg">
+                    Reviews
+                  </th>
+                  <th scope="col" className="px-6 py-3 rounded-e-lg"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {popularInstructors.map((item, index) => (
+                  <tr className="text-textPrimary" key={index}>
+                    <td className="px-6 py-4">
+                      <img className="h-12" src={item.img} alt="" />
+                    </td>
+                    <td className="px-6 py-4">
+                      <p>{item.name}</p>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center gap-2">
+                        <HiUsers />
+                        <p>{item.students}</p>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center gap-2">
+                        <BiSolidVideos />
+                        <p>{item.courses}</p>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className="flex items-center gap-2">
+                        <MdReviews />
+                        <p>{item.reviews}</p>
+                      </div>
+                    </td>
+                    <td className="px-6 py-4">
+                      <div className=" ">
+                        <button className="flex items-center gap-2 bg-gradient-to-r from-rgbFrom to-rgbTo px-10 py-2 rounded-md my-2">
+                          <span>Button</span>
+                        </button>
+                      </div>
+                    </td>
+                    <td>
+                      <div className=" ">
+                        <button className="flex items-center gap-2 bg-pink-700 px-9 py-2 rounded-md">
+                          <span>Button</span>
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </DashboardCard>
   );

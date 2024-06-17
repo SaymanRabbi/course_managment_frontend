787713c9bf7b21e27827443aaea9068ce077d83f
sayman rabbi <mdrabbi.asm@gmail.com>
2024-06-17 15:15:39 +0600

    make review part

--- src/Components/Dashboard/Reviews.tsx
+++ src/Components/Dashboard/Reviews.tsx
@@ -1,78 +1,73 @@
+import { useState } from "react";
 import DashboardCard from "./DashboardCard";
-import { ReviewData } from "../../dummyData/DummyData";
-import { IoStar } from "react-icons/io5";
-import { FaRegPenToSquare } from "react-icons/fa6";
-import { FaRegTrashCan } from "react-icons/fa6";
+// import { FaRegPenToSquare } from "react-icons/fa6";
+// import { FaRegTrashCan } from "react-icons/fa6";
+import { FaStar } from "react-icons/fa";
+import Button from "../Button/Button";
+import toast from "react-hot-toast";
 
 const Reviews = () => {
-  const reviewButton = [
-    {
-      icon: <FaRegPenToSquare className="text-[20px] " />,
-      text: "Edit",
-    },
-    {
-      icon: <FaRegTrashCan className="text-[20px] " />,
-      text: "Delete",
-    },
-  ];
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState("");
+  const handleRatingClick = (index: any) => {
+    setRating(index + 1);
+  };
+  const handleSubmitReview = () => {
+    if (rating === 0) {
+      return toast.error("Please Select Rating");
+    }
+    if (review === "") {
+      return toast.error("Please Write Review");
+    }
+    const data = {
+      rating,
+      review,
+    };
+    console.log(data);
+    toast.success("Review Submitted Successfully");
+  };
   return (
     <DashboardCard title="Reviews">
-      <div className=" grid grid-cols-12">
-        <div className=" col-span-12">
-          <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left ">
-              <thead className=" font-bold text-textPrimary text-[16px]">
-                <tr>
-                  <th scope="col" className="px-6 py-3 rounded-s-lg">
-                    Course Title
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Review
-                  </th>
-                  <th scope="col" className="px-6 py-3 rounded-e-lg"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {ReviewData.map((item, index) => (
-                  <tr className=" text-textPrimary" key={index}>
-                    <td className="px-6 py-4">{item.name}</td>
-                    <td className="px-6 py-4 flex gap-x-1 font-bold items-center">
-                      {[...Array(item.rating)].map((_, i) => {
-                        return (
-                          <span key={i}>
-                            {i < item.rating ? (
-                              <IoStar className="text-rgbTo" />
-                            ) : (
-                              <IoStar className="text-textSecondary" />
-                            )}
-                          </span>
-                        );
-                      })}
-                      {item.rating < 5 ? <h2>Good</h2> : <h2>Excellent</h2>}
-                    </td>
-                    <td className="px-6 py-4">
-                      <div className=" flex items-center gap-3">
-                        {reviewButton.map((item, index) => (
-                          <button
-                            className=" flex font-bold gap-1 
-                            hover:text-rgbTo hover:underline transition duration-300 ease-in-out
-                            "
-                            key={index}
-                          >
-                            <span>{item.icon}</span>
-
-                            {item.text}
-                          </button>
-                        ))}
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+      {/* Rating */}
+      <div>
+        <p className=" text-[16px] text-yellow-500 font-bold">Your Rating</p>
+        <div className=" flex gap-2 mt-3">
+          {
+            // Rating
+            [1, 2, 3, 4, 5].map((index) => (
+              <FaStar
+                key={index}
+                onClick={() => handleRatingClick(index)}
+                className={`text-2xl cursor-pointer font-bold ${
+                  index < rating ? "text-yellow-500" : "text-gray-300"
+                }`}
+              />
+            ))
+          }
         </div>
       </div>
+      {/* Rating */}
+      {/* review box */}
+      <div className=" mt-4">
+        <textarea
+          className=" primary_input text-gray-400 resize-none font-bold"
+          name="answer"
+          rows={3}
+          placeholder="Write Your Review Here..."
+          onChange={(e: any) => setReview(e?.target?.value)}
+        />
+      </div>
+      {/* review box */}
+      {/* submit button */}
+      <div className=" mt-3">
+        <Button
+          className="w-[30%]  bg-gradient-to-r from-rgbFrom to-rgbTo"
+          onClick={handleSubmitReview}
+        >
+          Submit Review
+        </Button>
+      </div>
+      {/* submit button */}
     </DashboardCard>
   );
 };

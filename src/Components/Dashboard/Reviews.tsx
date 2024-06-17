af7a637b2053681a5f28e1967cd309a8e8d3b2e6
sayman rabbi <mdrabbi.asm@gmail.com>
2024-06-17 15:20:58 +0600

    make rating func

--- src/Components/Dashboard/Reviews.tsx
+++ src/Components/Dashboard/Reviews.tsx
@@ -12,7 +12,8 @@ const Reviews = () => {
   const handleRatingClick = (index: any) => {
     setRating(index + 1);
   };
-  const handleSubmitReview = () => {
+  const handleSubmitReview = (e: any) => {
+    e.preventDefault();
     if (rating === 0) {
       return toast.error("Please Select Rating");
     }
@@ -24,49 +25,53 @@ const Reviews = () => {
       review,
     };
     console.log(data);
+    e.target.reset();
+    setRating(0);
     toast.success("Review Submitted Successfully");
   };
   return (
     <DashboardCard title="Reviews">
       {/* Rating */}
-      <div>
-        <p className=" text-[16px] text-yellow-500 font-bold">Your Rating</p>
-        <div className=" flex gap-2 mt-3">
-          {
-            // Rating
-            [1, 2, 3, 4, 5].map((index) => (
-              <FaStar
-                key={index}
-                onClick={() => handleRatingClick(index)}
-                className={`text-2xl cursor-pointer font-bold ${
-                  index < rating ? "text-yellow-500" : "text-gray-300"
-                }`}
-              />
-            ))
-          }
+      <form onSubmit={handleSubmitReview} className=" mt-3">
+        <div>
+          <p className=" text-[16px] text-yellow-500 font-bold">Your Rating</p>
+          <div className=" flex gap-2 mt-3">
+            {
+              // Rating
+              [1, 2, 3, 4, 5].map((index) => (
+                <FaStar
+                  key={index}
+                  onClick={() => handleRatingClick(index)}
+                  className={`text-2xl cursor-pointer font-bold ${
+                    index < rating ? "text-yellow-500" : "text-gray-300"
+                  }`}
+                />
+              ))
+            }
+          </div>
         </div>
-      </div>
-      {/* Rating */}
-      {/* review box */}
-      <div className=" mt-4">
-        <textarea
-          className=" primary_input text-gray-400 resize-none font-bold"
-          name="answer"
-          rows={3}
-          placeholder="Write Your Review Here..."
-          onChange={(e: any) => setReview(e?.target?.value)}
-        />
-      </div>
-      {/* review box */}
-      {/* submit button */}
-      <div className=" mt-3">
-        <Button
-          className="w-[30%]  bg-gradient-to-r from-rgbFrom to-rgbTo"
-          onClick={handleSubmitReview}
-        >
-          Submit Review
-        </Button>
-      </div>
+        {/* Rating */}
+        {/* review box */}
+        <div className=" mt-4">
+          <textarea
+            className=" primary_input text-gray-400 resize-none font-bold"
+            name="answer"
+            rows={3}
+            placeholder="Write Your Review Here..."
+            onChange={(e: any) => setReview(e?.target?.value)}
+          />
+        </div>
+        {/* review box */}
+        {/* submit button */}
+        <div className=" mt-3">
+          <Button
+            className="w-[30%]  bg-gradient-to-r from-rgbFrom to-rgbTo"
+            type="submit"
+          >
+            Submit Review
+          </Button>
+        </div>
+      </form>
       {/* submit button */}
     </DashboardCard>
   );
